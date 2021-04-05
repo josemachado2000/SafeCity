0796de8cf9538db7aadec853952e43a0eec568df
jpmachado2000 <jose.machado@ipvc.pt>
2021-04-05 19:49:27 +0100

    Updated Login actions and created shared preferences

--- app/src/main/java/commov/safecity/Login.kt
+++ app/src/main/java/commov/safecity/Login.kt
@@ -1,11 +1,14 @@
 package commov.safecity
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputLayout
 import commov.safecity.api.EndPoints
 import commov.safecity.api.LoginPostResponse
 import commov.safecity.api.ServiceBuilder
@@ -19,29 +22,61 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
-        val usernameEditText = findViewById<EditText>(R.id.login_username_editText)
-        val passwordEditText = findViewById<EditText>(R.id.login_password_editText)
+        val usernameTextInputLayout = findViewById<TextInputLayout>(R.id.login_username_TextInputLayout)
+        val passwordTextInputLayout = findViewById<TextInputLayout>(R.id.login_password_TextInputLayout)
 
         val buttonLogin = findViewById<Button>(R.id.login_buttonLogin)
         buttonLogin.setOnClickListener {
-            val username = usernameEditText.text.toString().trim()
-            val password = passwordEditText.text.toString().trim()
+            // Login Validations
+            val username = usernameTextInputLayout.editText?.text.toString().trim()
+            var validUsername = false
+            when {
+                username.isEmpty() -> {
+                    usernameTextInputLayout.error = getString(R.string.insert_note_ObligationError)
+                }
+                else -> {
+                    usernameTextInputLayout.error = ""
+                    validUsername = true
+                }
+            }
 
-            val request = ServiceBuilder.buildService(EndPoints::class.java)
-            val call = request.login(username = username, password = password)
+            val password = passwordTextInputLayout.editText?.text.toString().trim()
+            var validPassword = false
+            when {
+                password.isEmpty() -> {
+                    passwordTextInputLayout.error = getString(R.string.insert_note_ObligationError)
+                }
+                else -> {
+                    passwordTextInputLayout.error = ""
+                    validPassword = true
+                }
+            }
 
-            call.enqueue(object: Callback<LoginPostResponse> {
-                override fun onResponse(call: Call<LoginPostResponse>, response: Response<LoginPostResponse>) {
-                    if (response.isSuccessful) {
-                        val user: LoginPostResponse = response.body()!!
-                        Toast.makeText(this@Login, user.id.toString().plus(" -> NICE"), Toast.LENGTH_SHORT).show()
+            if (validUsername && validPassword) {
+                val request = ServiceBuilder.buildService(EndPoints::class.java)
+                val call = request.login(username = username, password = password)
+
+                call.enqueue(object: Callback<LoginPostResponse> {
+                    override fun onResponse(call: Call<LoginPostResponse>, response: Response<LoginPostResponse>) {
+                        if (response.isSuccessful) {
+                            val user: LoginPostResponse = response.body()!!
+                            Toast.makeText(this@Login, user.id.toString().plus(" -> NICE"), Toast.LENGTH_SHORT).show()
+
+                            val loginSharedPref: SharedPreferences = getSharedPreferences(getString(R.string.login_preference_file), Context.MODE_PRIVATE)
+                            with (loginSharedPref.edit()) {
+                                putBoolean(getString(R.string.logged), true)
+                                putString(getString(R.string.loggedUsername), username)
+                                apply()
+                            }
+                        }
                     }
-                }
 
-                override fun onFailure(call: Call<LoginPostResponse>, t: Throwable) {
-                    Toast.makeText(this@Login, "${t.message}", Toast.LENGTH_LONG).show()
-                }
-            })
+                    override fun onFailure(call: Call<LoginPostResponse>, t: Throwable) {
+                        Toast.makeText(this@Login, "${t.message}", Toast.LENGTH_LONG).show()
+                    }
+                })
+            }
+
 
         }
     }
