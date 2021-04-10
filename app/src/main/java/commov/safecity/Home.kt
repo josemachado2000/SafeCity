ec402e68122d2286a3f460a5964f123f906267f7
jpmachado2000 <jose.machado@ipvc.pt>
2021-04-10 18:24:48 +0100

    Updated build.gradle permissions and AndroidManifest.xml

--- app/src/main/java/commov/safecity/Home.kt
+++ app/src/main/java/commov/safecity/Home.kt
@@ -1,44 +1,86 @@
 package commov.safecity
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class Home : AppCompatActivity() {
+class Home : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home)
+        setContentView(R.layout.google_map)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
 
-//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-//        val loginSharedPref: SharedPreferences = getSharedPreferences(getString(R.string.login_preference_file), Context.MODE_PRIVATE)
-//        return if (loginSharedPref.getBoolean("logged", false)) {
-//            val inflater: MenuInflater = menuInflater
-//            inflater.inflate(R.menu.non_logged_menu, menu)
-//            true
-//        } else {
-//            val inflater: MenuInflater = menuInflater
-//            inflater.inflate(R.menu.logged_menu, menu)
-//            true
-//        }
-//    }
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
 
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        // Handle item selection
-//        return when (item.itemId) {
-//            R.id.login -> {
-//                val intent = Intent(this@Home, Login::class.java)
-//                startActivity(intent)
-//                true
-//            }
-//            R.id.logout -> {
-//                true
-//            }
-//            R.id.notes -> {
-//                val intent = Intent(this@Home, Notes::class.java)
-//                startActivity(intent)
-//                true
-//            }
-//            else -> super.onOptionsItemSelected(item)
-//        }
-//    }
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    // Menu
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val loginSharedPref: SharedPreferences = getSharedPreferences(getString(R.string.login_preference_file), Context.MODE_PRIVATE)
+        return if (loginSharedPref.getBoolean("logged", false)) {
+            val inflater: MenuInflater = menuInflater
+            inflater.inflate(R.menu.non_logged_menu, menu)
+            true
+        } else {
+            val inflater: MenuInflater = menuInflater
+            inflater.inflate(R.menu.logged_menu, menu)
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.login -> {
+                val intent = Intent(this@Home, Login::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.logout -> {
+                val loginSharedPref: SharedPreferences = getSharedPreferences(getString(R.string.login_preference_file), Context.MODE_PRIVATE)
+                with(loginSharedPref.edit()) {
+                    clear()
+                    apply()
+                }
+                true
+            }
+            R.id.notes -> {
+                val intent = Intent(this@Home, Notes::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
