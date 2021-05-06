0cdf5ad94ec7e7f495124064c36ac264e9bbf99b
jpmachado2000 <jose.machado@ipvc.pt>
2021-05-06 15:09:46 +0100

    Merged distanceFilter branch to development

--- app/src/main/java/commov/safecity/MarkerInfoWindow.kt
+++ app/src/main/java/commov/safecity/MarkerInfoWindow.kt
@@ -2,25 +2,10 @@ package commov.safecity
 
 import android.annotation.SuppressLint
 import android.app.Activity
-import android.content.Context
-import android.util.Log
 import android.view.View
 import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.appcompat.app.AlertDialog
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
-import commov.safecity.api.Anomaly
-import commov.safecity.api.EndPoints
-import commov.safecity.api.ServiceBuilder
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-
 
 class MarkerInfoWindow(context: Activity) : GoogleMap.InfoWindowAdapter {
     @SuppressLint("InflateParams")
